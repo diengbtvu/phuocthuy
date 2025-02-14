1d90ca74d341ac82fccacf88bcd026ad7a8508d3
diengbtvu <110122050@st.tvu.edu.vn>
2025-02-14 06:06:22 -0500

    first commit

---
+++ src/main/java/com/phuocthuy/PhuocthuyApplication.java
@@ -0,0 +1,13 @@
+package com.phuocthuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhuocthuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhuocthuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phuocthuy/PhuocthuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuocthuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhuocthuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
